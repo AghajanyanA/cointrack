cbcdcc279bd825f97d49bc5ca60c6a665428077e
ArmA0 <armenworkplace@gmail.com>
2022-11-28 18:00:00 +0400

    added tables with charts

---
+++ src/components/pages/3LivePrices/Charts/Charts.tsx
@@ -0,0 +1,30 @@
+import { Line, LineChart, YAxis } from "recharts";
+
+const Charts = ({ sparkline }: any) => {
+  const prices = sparkline.map((price: any) => ({ price }));
+  console.log(prices);
+
+  const hasRisen = prices[0].price < prices[prices.length -1].price
+
+  return (
+    <>
+      <LineChart
+        width={150}
+        height={30}
+        data={prices}
+        margin={{
+          top: 5,
+          right: 0,
+          left: 10,
+          bottom: 0,
+        }}
+      >
+        <YAxis hide={true} domain={["dataMin", "dataMax"]} />
+
+        <Line dot={false} dataKey="price" stroke={hasRisen ? '#32D25A' : '#FF0000'} />
+      </LineChart>
+    </>
+  );
+};
+
+export default Charts;
--- src/components/pages/3LivePrices/LivePrices.tsx
+++ src/components/pages/3LivePrices/LivePrices.tsx
@@ -1,13 +1,34 @@
+import { useAppSelector } from '../../../redux/hooks';
 import s from './LivePrices.module.css'
+import { marketsCoins } from '../../../types'
+import Charts from './Charts/Charts';
 
 const LivePrices = () => {
 
 
+    const marketsData: marketsCoins[] = useAppSelector(state => state.marketsReducer.markets)
+
+
     return <div className={s.wrapper}>
         <h2 className={s.h2} >See live cryptocurrency prices</h2>
-        <div className={s.table}>
-            
-        </div>
+        <table className={s.table}>
+            <tr className={s.tHeader}>
+                <th>Name</th>
+                <th>Price</th>
+                <th>1 Hour Change</th>
+                <th>Market Cap</th>
+                <th>Price Graph (7D)</th>
+                <th>btn</th>
+            </tr>
+                {marketsData.map(coin => <tr className={s.mappedTr}>
+                    <td><img width='22px' src={coin.image} alt='coin logo' />{coin.name}</td>
+                    <td>{coin.current_price.toFixed(2)}</td>
+                    <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+                    <td>{coin.market_cap.toFixed(2)}</td>
+                    <td>{<Charts sparkline={coin.sparkline_in_7d.price} />}</td>
+                    <td>...</td>
+                </tr>)}
+        </table>
     </div>
 }
  
