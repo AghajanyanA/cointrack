4214f77f7d33842ad196aa0e3d4278ed1e0ede45
AghajanyanA <astara@null.net>
2023-02-03 03:57:59 +0400

    Display minus before dollar sign

--- src/components/pages/1TrackCrypto/Card/Card.tsx
+++ src/components/pages/1TrackCrypto/Card/Card.tsx
@@ -14,6 +14,7 @@ const Card = ({loading, error, item}:CardProps) => {
     }))
 
     const hasRisen = sparkline_in_7d.price[0] < sparkline_in_7d.price[sparkline_in_7d.price.length - 1] 
+    const priceChange24hModifiedMinus = price_change_24h < 0 ? `-$${Number(price_change_24h.toString().slice(1)).toFixed(2)}` : `$${Number(price_change_24h.toFixed(2))}`
     
     return <div className={s.wrap} >
         {loading && <p>Loading...</p>}
@@ -25,7 +26,7 @@ const Card = ({loading, error, item}:CardProps) => {
             <div className={s.dataWrap}>
                     <p className={s.name}>{name}</p>
                     <p className={s.current_price}>${current_price.toFixed(2)}</p>
-                    <p className={`${s.price_change_24h } ${price_change_24h >= 0 ? s.green : s.red}`}>${price_change_24h.toFixed(2)}</p>
+                    <p className={`${s.price_change_24h } ${price_change_24h >= 0 ? s.green : s.red}`}>{priceChange24hModifiedMinus}</p>
             </div>
             
             <div className={s.rightSide} >
@@ -48,11 +49,11 @@ const Card = ({loading, error, item}:CardProps) => {
                     <defs>
                         <linearGradient id="chartUp" x1="0" y1="0" x2="0" y2="1" >
                             <stop offset="0%" stopColor="#fb00e262" />
-                            <stop offset="100%" stopColor="#fb00e21f"  /> 
+                            <stop offset="100%" stopColor="#fb00e210"  /> 
                         </linearGradient>
                         <linearGradient id="chartDown" x1="0" y1="0" x2="0" y2="1" >
                             <stop offset="0%" stopColor="#ff000094" />
-                            <stop offset="100%" stopColor="#ff000038"  /> 
+                            <stop offset="100%" stopColor="#ff000010"  /> 
                         </linearGradient>
                     </defs>
                     <YAxis 
@@ -63,9 +64,9 @@ const Card = ({loading, error, item}:CardProps) => {
                         dataKey="date"
                         hide={true}
                     />
-                    <Area type="monotone" dataKey="price" stroke={hasRisen ? '#fb00e21f' : 'red'}
+                    <Area type="monotone" dataKey="price" strokeWidth={1.5} stroke={hasRisen ? '#805AD5' : '#FF0000'}
                         fill={hasRisen
-                        ? "url(#chartUp)" : 'url(#chartDown)'} radius={25} />
+                        ? "url(#chartUp)" : 'url(#chartDown)'} radius='25' />
                 </AreaChart>
             </ResponsiveContainer>
         </div>
