d169a00df8b3607194c9be01b18a83918e8d5e5f
AghajanyanA <astara@null.net>
2023-02-25 21:00:10 +0400

    changed table structure

--- src/components/pages/3LivePrices/LivePrices.tsx
+++ src/components/pages/3LivePrices/LivePrices.tsx
@@ -18,30 +18,26 @@ const LivePrices = () => {
 
     return <div className={s.wrapper}>
         <h2 className={s.h2}>See live cryptocurrency prices</h2>
-        <table className={s.table}>
-            <thead>
-                <tr className={s.tHeader}>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>1 Hour Change</th>
-                    <th>Market Cap</th>
-                    <th>Price Graph (7D)</th>
-                    <th>btn</th>
-                </tr>
-            </thead>
-            <tbody>
+        <div className={s.table}>
+            <div className={`${s.tableHeader} ${s.tablePositioning}`}>
+                <p>Name</p>
+                <p>Price</p>
+                <p>1 Hour Change</p>
+                <p>Market Cap</p>
+                <p>Price Graph (7D)</p>
+            </div>
+            <div className={s.tableBody}>
                 {!marketsData.length ? <div className={s.loading}>Loading</div> : null}
-                {marketsData.map(coin => <tr key={coin.id} className={s.mappedTr}>
-                    <td><img width='22px' src={coin.image} alt='coin logo' />{coin.name}</td>
-                    <td>{coin.current_price.toFixed(2)}</td>
-                    <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-                    <td>{coin.market_cap.toFixed()}</td>
-                    <td>{<Charts sparkline={coin.sparkline_in_7d.price} />}</td>
-                    <td>...</td>
-                </tr>)}
-            </tbody>
-        </table>
-        <div className={s.loadMore}><Button>Load more</Button></div>
+                {marketsData.map(coin => <div key={coin.id} className={s.mappedTr}>
+                    <p><img width='22px' src={coin.image} alt='coin logo' className={s.logo} />{coin.name}</p>
+                    <p>{coin.current_price.toFixed(2)}</p>
+                    <p>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+                    <p>{coin.market_cap.toFixed()}</p>
+                    <p>{<Charts sparkline={coin.sparkline_in_7d.price} />}</p>
+                </div>)}
+            </div>
+        </div>
+        <div className={s.button}><Button>Load more</Button></div>
     </div>
 }
  
